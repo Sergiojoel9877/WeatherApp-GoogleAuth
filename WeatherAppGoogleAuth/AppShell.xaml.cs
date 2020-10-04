3629817f9b50e33bd58edbb2492dba3876c5cc8e
Sergio Joel Ferreras Batista <sferreras@sweatworks.net>
2020-10-04 18:12:40 -0400

    Create base project.

---
+++ WeatherAppGoogleAuth/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WeatherAppGoogleAuth.ViewModels;
+using WeatherAppGoogleAuth.Views;
+using Xamarin.Forms;
+
+namespace WeatherAppGoogleAuth
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ WeatherAppGoogleAuth/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using WeatherAppGoogleAuth.ViewModels;
+
+namespace WeatherAppGoogleAuth.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
