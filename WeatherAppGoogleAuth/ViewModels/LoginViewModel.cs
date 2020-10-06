0b66f63ceb007d9f68ed9f955dafa90dd55bb4e0
Sergio Joel Ferreras Batista <sferreras@sweatworks.net>
2020-10-06 11:58:03 -0400

    Update LoginViewModel.cs
    Delete templated code.

--- WeatherAppGoogleAuth/ViewModels/LoginViewModel.cs
+++ WeatherAppGoogleAuth/ViewModels/LoginViewModel.cs
@@ -6,21 +6,12 @@ using Xamarin.Forms;
 
 namespace WeatherAppGoogleAuth.ViewModels
 {
-    public class LoginViewModel : BaseViewModel
     public class LoginViewModel : INotifyPropertyChanged
     {
         public Command LoginCommand { get; }
 
-        public LoginViewModel()
         {
-            LoginCommand = new Command(OnLoginClicked);
-        }
 
-        private async void OnLoginClicked(object obj)
-        {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
-        }
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
