133fcab4b51e1c729ad865f298dcf604ed9f9088
Sergio Joel Ferreras Batista <sferreras@sweatworks.net>
2020-10-06 12:02:08 -0400

    Create WeatherPage.xaml.cs

---
+++ WeatherAppGoogleAuth/Views/WeatherPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Sharpnado.Tasks;
+using Splat;
+using WeatherAppGoogleAuth.ViewModels;
+using Xamarin.Forms;
+
+namespace WeatherAppGoogleAuth.Views
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+            BindingContext = Locator.Current.GetService<WeatherPageViewModel>();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            TaskMonitor.Create(((WeatherPageViewModel)BindingContext).GetWeatherData.Value.ExecuteAsync());
+        }
+    }
+}
